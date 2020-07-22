1dd68429c8772809e1e44d45ab8372ae1f5a547e
StevenP17 <steven.pfuendlin@oxid-esales.com>
2020-07-22 04:30:14 +0000

    initial commit

---
+++ Event/LoggerEventSubscriber.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace OxidAcademy\CategoryLogger\Event;
+
+use OxidEsales\Eshop\Core\Registry;
+use OxidEsales\Eshop\Application\Model\Category;
+use OxidEsales\EshopCommunity\Internal\Framework\Event\AbstractShopAwareEventSubscriber;
+use OxidEsales\EshopCommunity\Internal\Transition\ShopEvents\AfterModelInsertEvent;
+use OxidEsales\EshopCommunity\Internal\Transition\ShopEvents\AfterModelDeleteEvent;
+use OxidEsales\EshopCommunity\Internal\Transition\ShopEvents\AfterModelUpdateEvent;
+use Symfony\Component\EventDispatcher\Event;
+use Psr\Log\LoggerInterface;
+
+class LoggerEventSubscriber extends AbstractShopAwareEventSubscriber
+{
+    private LoggerInterface $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function logCreation(Event $event): void
+    {
+        $model = $event->getModel();
+
+        if ($this->isCategory($model) === true) {
+            $this->logger->info('Category with ID ' . $model->getId() . ' was created.');
+        }
+    }
+
+    public function logDeletion(Event $event): void
+    {
+        $model = $event->getModel();
+
+        if ($this->isCategory($model) === true) {
+            $this->logger->info('Category with ID ' . $model->getId() . ' was deleted.');
+        }
+    }
+
+    public function logUpdate(Event $event): void
+    {
+        $model = $event->getModel();
+
+        if ($this->isCategory($model) === true) {
+            $this->logger->info('Category with ID ' . $model->getId() . ' was updated.');
+        }
+    }
+
+    private function isCategory(Object $model): bool
+    {
+        if (get_class($model) == get_class(oxNew(Category::class))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            AfterModelInsertEvent::NAME => 'logCreation',
+            AfterModelDeleteEvent::NAME => 'logDeletion',
+            AfterModelUpdateEvent::NAME => 'logUpdate',
+        ];
+    }
+}
---
+++ metadata.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '2.1';
+
+/**
+ * Module information
+ */
+$aModule = [
+    'id'          => 'oxaccategorylogger',
+    'title'       => [
+        'de' => 'OXAC Kategorie Logger',
+        'en' => 'OXAC Category Logger',
+    ],
+    'description' => [
+        'de' => 'Erfasst Kategorie Erstellung, Löschung und Aktualisierung.',
+        'en' => 'Logs category creation, deletion and update.',
+    ],
+    'thumbnail'   => '',
+    'version'     => '0.1.0',
+    'author'      => 'OXID Academy',
+    'url'         => 'https://www.oxid-esales.com/',
+    'email'       => 'academy@oxid-esales.com',
+];
